4170ed955eed5dfa2fe7302530c2536aa4a11209
cynthiawambuamumbua <cynthiawambuamumbua@gmail.com>
2023-04-15 20:56:24 +0300

    first commit

---
+++ args.py
@@ -0,0 +1,28 @@
+# Create a function to print Welcome ( without any return data to main body )
+def greet(*greet):
+    print(f"greet")
+
+# Create a function to display sum of two integers, print should be inside the function.
+def sum(a,b):
+ return a+b
+   
+
+# Create a function to display square of 5 ( without any return data to parent script )
+def squares(int):
+   number=5
+   answer=squares(int)
+   print(answer)
+
+# Take one user input number and pass it to a functTake one user input number 
+# and pass it to a function to display the same by adding 10 to its value.
+num=input("Enter number: ")
+print(num+10)
+
+# //Write a function that takes a string as input and 
+# returns a new string with the first letter of each sentence capitalized.
+def string():
+    newstring=""
+    newstring.title()
+    return newstring
+    
+  
